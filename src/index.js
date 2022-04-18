62f6a5860585102b797cd5409592520b8070bab5
Chipskein <abfn0905@gmail.com>
2022-04-18 12:24:05 -0300

    fix: styles & preparing files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const express=require('express');
 const session=require('express-session');
 const app=express();
 const port=process.env.PORT || 8080;
-
 //config session
 app.use(session({
     secret: 'ajfsahfjsahfjkhafjkhsajkfhasfhjksafhjksafhjksafhjksa',
@@ -14,9 +13,9 @@ app.use(session({
 }))
 
 //routes
-const mangaRoutes=require("./routes/manga-routes.js");
-const userRoutes=require("./routes/user-routes.js");
-const reviewsRoutes=require("./routes/reviews-routes.js");
+const userRoutes=require("./routes/userRoutes.js");
+const messageRoutes=require("./routes/messageRoutes.js");
+const groupRoutes=require("./routes/groupRoutes.js");
 
 //set view engine
 app.set('view engine','ejs');
@@ -32,11 +31,10 @@ app.use('*', (req, res, next) => {
     console.log(`REQUEST:${new Date().toISOString()} ~ ${req.method} ${req.baseUrl} `);
     next();
 })
-
 app.use('/users',userRoutes);
-app.use('/manga',mangaRoutes);
-app.use('/review',reviewsRoutes);
-app.get('/',(req,res)=>res.redirect('/manga'));
+app.use('/groups',groupRoutes);
+app.use('/message',messageRoutes);
+app.get('/',(req,res)=>res.redirect('/groups'));
 app.use('*', (req, res) => res.redirect('/NotFound.html'));
 
 app.listen(port,()=>console.log(`SERVER STARTED ON ${port}`));
