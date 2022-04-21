23aedea0f9204c284a662ab63f1e7a2bc16448fd
Chipskein <abfn0905@gmail.com>
2022-04-20 22:13:05 -0300

    feat:list group e pagination

--- src/controller/groupController.js
+++ src/controller/groupController.js
@@ -1,11 +1,14 @@
 const { hashSync,compareSync} = require('bcrypt');
 const {customAlphabet }=require('nanoid');
 const nanoid=customAlphabet('1234567890',8);
-const groupModel=require('../model/groupModel');
+const {GroupDAO,GroupModel}=require('../model/groupModel');
 class GroupController{
-  static showGroupAll(req,res){
-    console.log(req.session.user)
-    return res.render('group/showGroupAll',{user:req.session.user})
+  static async showGroupAll(req,res){
+    let {offset}=req.query;
+    if(!offset) offset=0;
+    const total=await GroupDAO.countTotalGroups();
+    const groups=await GroupDAO.getAllGroups(offset);
+    return res.render('group/showGroupAll',{user:req.session.user,groups:groups,countGroups:total})
   }
 }
 module.exports=GroupController;
---
+++ src/model/groupModel.js
@@ -0,0 +1,60 @@
+const { dbcon } = require("../database/connection");
+class GroupModel {
+    constructor(id,name,adminid,img) {
+        this.id = id;
+        this.name = name;
+        this.adminid = adminid;
+        this.img = img;
+        this.created_at = new Date().toISOString();
+        this.updated_at = new Date().toISOString();
+        this.activate = 'true';
+    }
+}
+
+// DAO = DATA ACCESS OBJECT
+class GroupDAO {
+
+    /*
+    static async registergroup(group) {
+          
+        const sql = 'INSERT INTO public.groups (name,email,password,img) VALUES ($1, $2, $3, $4) RETURNING id;';
+        const values = [group.name, group.email, group.password, group.img];
+        try {
+            await dbcon.query(sql, values);
+        } catch (error) {
+            console.log('Error groupDAO.registergroup',{ error });
+        }
+    }
+    */
+    static async getAllGroups(offset=0,limit=5) {
+        const sql = `
+            select 
+                g.id,
+                g.img,
+                g.name,
+                g.adminid,
+                g.created_at,
+                g.updated_at,
+                g.activate,
+                (select count(*) from group_members gm where gm.groupid=g.id) as memberqt,
+                array_agg(gm2.userid) as users
+            from "groups" g
+            left join group_members gm2 on gm2.groupid=g.id 
+            group by g.id
+            limit ${limit}
+            offset ${offset};
+        `;
+        const result = await dbcon.query(sql);
+        return result.rows;
+    }
+    static async countTotalGroups(){
+        const sql = `select count(*) as qt from "groups" g;`;
+        const result = await dbcon.query(sql);
+        return result.rows[0].qt;
+    }
+}
+
+module.exports = {
+    GroupModel,
+    GroupDAO
+};
